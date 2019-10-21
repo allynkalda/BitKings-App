aa9b3af72d9858e165a3316f52f7a9f78ff0e789
Allyn Alda <Teddy@om.home>
2019-10-21 21:01:17 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Faq from './components/Faq';
+import Pricing from './components/Pricing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BitKings</h1>
+      <Switch>
+        <Route path="/faq" component={Faq}/>
+        <Route path="/pricing" component={Pricing}/>
+      </Switch>
     </div>
   );
 }
