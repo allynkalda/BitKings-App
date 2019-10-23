5b34bc4cb1c10aaa366365b3194271f05bc809ec
Allyn <allyn.alda@gmail.com>
2019-10-23 20:53:39 +0200

    added data

---
+++ src/data/faq.js
@@ -0,0 +1,57 @@
+const faq = {
+    "data": [
+        {
+          "index": 1,
+          "title": "I am a beginner",
+          "questions": [
+            "How do I register on BitKings Exchange?",
+            "How do I purchase crypto with my credit card on BitKings Exchange?",
+            "How do I Deposit to BitKings Exchange?",
+            "How do I trade on BitKings Exchange?",
+            "How do I Withdraw from BitKings Exchange?",
+            "How Do I create an API?",
+            "Referral Program and Bounty Programs",
+            ]
+        },
+        {
+        "index": 2,
+        "title": "Account Access",
+        "questions": [
+          "How do I verify my account?",
+          "What are the different verification levels?",
+          "Can I verify a corporate account?",
+          "How do I unlock my account?"
+          ]
+        },
+        {
+        "index": 3,
+        "title": "Secure Account",
+        "questions": [
+            "What is a Two-Factor Authentication?",
+            "How do I set up my 2 Factor Identification (2FA)?",
+            "How do I disable my Two-Factor Authentication?",
+            "How do I deal with a 2FA Error?",
+            "How does BitKings Exchange secure my personal information?",
+            "How do I secure my account?"
+            ]
+        },
+        {
+        "index": 4,
+        "title": "Deposits and Withdrawals",
+        "questions": [
+          "How can I deposit wrongly?",
+          "What happens when I withdraw to the wrong address?",
+          "Why am I not receiving emails from BitKings Exchange?"
+          ]
+        },
+        {
+        "index": 5,
+        "title": "Trading",
+        "questions": [
+          "What is the difference between market and limit trading?",
+          "What factors should I consider before making a trade?",
+          "What tools presented on BitKings Exchange assist my trading?"
+          ]
+        },
+      ]
+  }
