35fc0683f3a8b77142ff1fb1c4eef21180f97c1d
Allyn Alda <Teddy@om.home>
2019-10-24 21:37:44 +0200

    Updates

---
+++ precache-manifest.bab10dd99ad48de456a75c8ef90e9e01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9915e32b8aea5ccb16aa4df9f488ebe",
+    "url": "/BitKings-App/index.html"
+  },
+  {
+    "revision": "ee9b75bf453b29390872",
+    "url": "/BitKings-App/static/css/main.5fb65f45.chunk.css"
+  },
+  {
+    "revision": "9a4ba09ed6e4851c8b06",
+    "url": "/BitKings-App/static/js/2.d226a3e2.chunk.js"
+  },
+  {
+    "revision": "ee9b75bf453b29390872",
+    "url": "/BitKings-App/static/js/main.f437cec4.chunk.js"
+  },
+  {
+    "revision": "3dee83ef49266e860644",
+    "url": "/BitKings-App/static/js/runtime-main.6641c1be.js"
+  }
+]);
